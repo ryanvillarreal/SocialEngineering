635ad24ed06d6d0b04cfc780d4e9f5d4996f11b5
Ryan Villarreal <999886+ryanvillarreal@users.noreply.github.com>
2018-12-05 16:50:14 -0500

    Update gophish_changepassword.sh

--- PhishStaging/scripts/gophish_changepassword.sh
+++ PhishStaging/scripts/gophish_changepassword.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Color Reset
-Color_Off='\033[0m'
-# Color Green
-Green='\033[0;32m'
-
 # change the password of the GoPhish server to something random and output to the screen.
 sudo apt -qq install -y sqlite apache2-utils
 
@@ -13,7 +8,7 @@ password=`sudo head /dev/urandom | tr -dc A-Za-z | head -c 13; echo ''`
 #password="QDLAQWTUBOHLM"
 
 hash=`htpasswd -bnBC 10 "" "$password" | tr -d ':\n' | sed 's/$2y/$2a/'`
-echo "$Green $password $Color_off"
+echo "$password"
 
 # place it in the SQLite Database
 sudo sqlite3 /opt/gophish/gophish.db "update users set hash='$hash' where username='admin';"
